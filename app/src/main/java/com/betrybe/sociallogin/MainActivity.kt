f56f161327e03b47997b00fbd244d63d38392b5b
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-11-10 21:42:52 -0300

    feat(Req04): created login button disabled

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.betrybe.sociallogin
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private val loginButton: Button by lazy { findViewById(R.id.login_button) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginButton.isEnabled = false
     }
 }
