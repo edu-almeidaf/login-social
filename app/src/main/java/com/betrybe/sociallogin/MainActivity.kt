ead8c97bd62dc4d1ab708d2f09dcfa56d84dd644
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-11-11 13:48:35 -0300

    feat(Req08 and Req09): Created email and password fields validation

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -5,34 +5,51 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
 
     private val loginButton: Button by lazy { findViewById(R.id.login_button) }
-    private val emailInput: TextInputEditText by lazy { findViewById(R.id.email_text_input_field) }
-    private val passwordInput: TextInputEditText by lazy { findViewById(R.id.password_text_input_field) }
+
+    private val emailInput: TextInputLayout by lazy { findViewById(R.id.email_text_input_layout) }
+    private val emailInputField: TextInputEditText by lazy { findViewById(R.id.email_text_input_field) }
+
+    private val passwordInput: TextInputLayout by lazy { findViewById(R.id.password_text_input_layout) }
+    private val passwordInputField: TextInputEditText by lazy { findViewById(R.id.password_text_input_field) }
+
+    private val regexEmail = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\$"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         enableButton()
-        validateEmail()
+        loginButton.setOnClickListener { validateEmailAndPassword() }
     }
 
     private fun enableButton () {
         loginButton.isEnabled = false
-        emailInput.doOnTextChanged { text, start, before, count -> verifyFields() }
-        passwordInput.doOnTextChanged { text, start, before, count -> verifyFields() }
+        emailInputField.doOnTextChanged { text, start, before, count -> verifyFields() }
+        passwordInputField.doOnTextChanged { text, start, before, count -> verifyFields() }
     }
 
     private fun verifyFields() {
-        val emailInputNotEmpty = emailInput.text?.isNotEmpty()!!
-        val passwordInputNotEmpty = passwordInput.text?.isNotEmpty()!!
+        val emailInputNotEmpty = emailInputField.text?.isNotEmpty()!!
+        val passwordInputNotEmpty = passwordInputField.text?.isNotEmpty()!!
         loginButton.isEnabled = emailInputNotEmpty && passwordInputNotEmpty
     }
 
-    private fun validateEmail() {
+    private fun validateEmailAndPassword() {
+        val emailText = emailInputField.text.toString()
+        val passwordText = passwordInputField.text.toString()
 
+        if (!emailText.matches(regexEmail.toRegex())) {
+            emailInput.error = "Email inválido"
+        } else if (passwordText.length < 4) {
+            passwordInput.error = "Senha deve ter mais de 4 caracteres"
+        } else {
+            emailInput.error = null
+            passwordInput.error = null
+        }
     }
 }
