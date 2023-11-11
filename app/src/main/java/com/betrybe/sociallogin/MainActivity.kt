d7f2ee787196afc3f05bf5ed4b07575c0dce8f14
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-11-11 13:26:13 -0300

    refactor(Req07): Added id to email and password fields

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -1,36 +1,38 @@
 package com.betrybe.sociallogin
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
-import com.google.android.material.textfield.TextInputLayout
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
 
     private val loginButton: Button by lazy { findViewById(R.id.login_button) }
-    private val emailInput: TextInputLayout by lazy { findViewById(R.id.email_text_input_layout) }
-    private val passwordInput: TextInputLayout by lazy { findViewById(R.id.password_text_input_layout) }
+    private val emailInput: TextInputEditText by lazy { findViewById(R.id.email_text_input_field) }
+    private val passwordInput: TextInputEditText by lazy { findViewById(R.id.password_text_input_field) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         enableButton()
+        validateEmail()
     }
 
     private fun enableButton () {
         loginButton.isEnabled = false
-        emailInput.editText?.doOnTextChanged { text, start, before, count -> verifyFields() }
-        passwordInput.editText?.doOnTextChanged { text, start, before, count -> verifyFields() }
+        emailInput.doOnTextChanged { text, start, before, count -> verifyFields() }
+        passwordInput.doOnTextChanged { text, start, before, count -> verifyFields() }
     }
 
     private fun verifyFields() {
-        val emailInputNotEmpty = emailInput.editText?.text?.isNotEmpty()!!
-        val passwordInputNotEmpty = passwordInput.editText?.text?.isNotEmpty()!!
-
+        val emailInputNotEmpty = emailInput.text?.isNotEmpty()!!
+        val passwordInputNotEmpty = passwordInput.text?.isNotEmpty()!!
         loginButton.isEnabled = emailInputNotEmpty && passwordInputNotEmpty
+    }
+
+    private fun validateEmail() {
+
     }
 }
